7eb918d4b167f09becbe337f6ca009ee188762ff
xpipi2019 <1193820217@qq.com>
2024-09-29 19:20:47 +0800

    uploaded

--- HW2/HW02_08.java
+++ HW2/HW02_08.java
@@ -2,4 +2,17 @@
  * @author XPIPI
  */
 public class HW02_08 {
+    public static void main(String[] args) {
+        int now = 0,time = 0;
+        while (true) {
+            now += 4;
+            time++;
+            if (now >= 100) {
+                break;
+            } else {
+                now -= 1;
+            }
+        }
+        System.out.println(time);
+    }
 }
