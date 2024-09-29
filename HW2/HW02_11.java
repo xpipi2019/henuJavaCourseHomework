ebff486037e54098b426c6b088ceeff6274c38f4
xpipi2019 <1193820217@qq.com>
2024-09-29 21:32:18 +0800

    fixed

---
+++ HW2/HW02_11.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * @author XPIPI
+ */
+class myMax {
+    static int findMax(int x,int y){
+        return x>y?x:y;
+    }
+    static double findMax(double x,double y){
+        return x>y?x:y;
+    }
+    static double findMax(double x,double y,double z){
+        return (x>y ? x:y) > z ? (x>y ? x:y):z;
+    }
+}
+
+public class HW02_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("输入两个整数：");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("最大值为："+myMax.findMax(x1,y1));
+
+        System.out.println("输入两个小数：");
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+        System.out.println("最大值为："+myMax.findMax(x2,y2));
+
+        System.out.println("输入三个小数：");
+        double x3 = sc.nextDouble();
+        double y3 = sc.nextDouble();
+        double z3 = sc.nextDouble();
+        System.out.println("最大值为："+myMax.findMax(x3,y3,z3));
+    }
+}
---
+++ HW2/HW02_13.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * @author XPIPI
+ */
+public class HW02_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入矩阵行数: ");
+        int rows = sc.nextInt();
+        System.out.println("输入矩阵列数: ");
+        int cols = sc.nextInt();
+        int[][] a = new int[rows][cols];
+        int[][] b = new int[cols][rows];
+
+        System.out.println("输入矩阵：");
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                b[j][i] = a[i][j];
+            }
+        }
+        //转置操作
+        System.out.println("矩阵转置后为 ");
+        for(int i=0;i<cols;i++)
+        {
+            for(int j=0;j<rows;j++)
+            {
+                System.out.print(b[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
