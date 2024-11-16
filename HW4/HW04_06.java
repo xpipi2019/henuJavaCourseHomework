314490b15666d37ab5c8a916c0ebbb3f29cb6cad
xpipi2019 <1193820217@qq.com>
2024-11-16 18:57:55 +0800

    HW04_06 use lambda

--- HW4/HW04_06.java
+++ HW4/HW04_06.java
@@ -7,12 +7,8 @@ interface Inner {
 
 class Outer {
     public static Inner method() {
-        return new Inner() {
-            @Override
-            public void introduce() {
-                System.out.println("实现了Inner接口的匿名内部类！");
-            }
-        };
+        return () -> System.out.println("实现了Inner接口的匿名内部类！");
+        //lambda
     }
 }
 
